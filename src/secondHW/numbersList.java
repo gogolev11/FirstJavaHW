e9fb7061a54c4a847bdb99b23eec0280d9cfbb4d
n.gogolev <NikitaG119865912>
2021-11-29 16:46:35 +0300

    задача про последовательность четырёхзначных

---
+++ src/secondHW/numbersList.java
@@ -0,0 +1,10 @@
+package secondHW;
+
+public class numbersList {
+    public static void main(String[] args) {
+        int number=997;
+        while (number<9997) {
+            System.out.println(number+=3);
+        }
+    }
+}
