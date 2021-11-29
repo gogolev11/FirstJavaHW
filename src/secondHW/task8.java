b1dabee6b654ac293efeb8bde325c0359c4224f3
Nikita <gnm11@yandex.ru>
2021-11-29 21:31:21 +0300

    от 90 до 0

---
+++ src/secondHW/task8.java
@@ -0,0 +1,11 @@
+package secondHW;
+
+public class task8 {
+    public static void main(String[] args) {
+        int n=90;
+        do {
+            System.out.println(n);
+            n-=5;
+        } while (n>=5);
+    }
+}
