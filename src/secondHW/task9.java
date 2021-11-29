d08d4a8cf7a80cca0b174df043f5ba1b8cccb73f
Nikita <gnm11@yandex.ru>
2021-11-29 21:59:42 +0300

    степени двух

---
+++ src/secondHW/task9.java
@@ -0,0 +1,13 @@
+package secondHW;
+
+import java.lang.module.ResolutionException;
+
+public class task9 {
+    public static void main(String[] args) {
+        int n=2;
+        for (int i=1;i<21;i++) {
+            System.out.println(Math.pow(n,i));
+        }
+
+    }
+}
