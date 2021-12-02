bf1468a40674bbc80db217d9752d5e5b33082a82
n.gogolev <NikitaG119865912>
2021-12-02 17:30:27 +0300

    3 HW 3 Part final

--- src/thirdHW/Bubble.java
+++ src/thirdHW/Bubble.java
@@ -4,7 +4,7 @@ public class Bubble {
     int min=Integer.MIN_VALUE;
     void bubbleSort(int array[]) {
         for (int k=array.length;k>0;k--){
-            for (int i=0;i<array[k-1];i++) {
+            for (int i=0;i<(k-1);i++) {
                 if (array[i]>array[i+1]) {
                     min=array[i+1];
                     array[i+1]=array[i];
