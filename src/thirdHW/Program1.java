0cfa60324091daa70aa492b8e5feb03da08e8134
n.gogolev <NikitaG119865912>
2021-12-02 15:04:00 +0300

    3 HW 1 Part final

--- src/thirdHW/Program1.java
+++ src/thirdHW/Program1.java
@@ -7,17 +7,63 @@ public class Program1 {
         Calculator calc =new Calculator();
         Scanner sc =new Scanner(System.in);
         System.out.println("Введите номер операции");
+        System.out.println("1.Сложение");
+        System.out.println("2.Деление");
+        System.out.println("3.Умножение");
+        System.out.println("4.Вычетание");
+        int calcType = sc.nextInt();
+        System.out.println("Введите цифры для вычисления");
         if (sc.hasNextInt()) {
-            int calcType = sc.nextInt();
-            System.out.println("Введите цифры для вычисления");
+            int num1 = sc.nextInt();
+            int num2 = sc.nextInt();
+            switch (calcType) {
+                case 1:
+                    System.out.println(calc.add(num1, num2));
+                    break;
+                case 2:
+                    System.out.println(calc.div(num1, num2));
+                    break;
+                case 3:
+                    System.out.println(calc.multipli(num1, num2));
+                    break;
+                case 4:
+                    System.out.println(calc.subtr(num1, num2));
+            }
         } else if (sc.hasNextDouble()) {
-            double calcType =sc.nextDouble();
+            double num1 =sc.nextDouble();
+            double num2 =sc.nextDouble();
+            switch (calcType) {
+                case 1:
+                    System.out.println(calc.add(num1, num2));
+                    break;
+                case 2:
+                    System.out.println(calc.div(num1, num2));
+                    break;
+                case 3:
+                    System.out.println(calc.multipli(num1, num2));
+                    break;
+                case 4:
+                    System.out.println(calc.subtr(num1, num2));
+            }
         } else if (sc.hasNextLong()) {
-            long calcType =sc.nextLong();
+            long num1 =sc.nextLong();
+            long num2 =sc.nextLong();
+            switch (calcType) {
+                case 1:
+                    System.out.println(calc.add(num1, num2));
+                    break;
+                case 2:
+                    System.out.println(calc.div(num1, num2));
+                    break;
+                case 3:
+                    System.out.println(calc.multipli(num1, num2));
+                    break;
+                case 4:
+                    System.out.println(calc.subtr(num1, num2));
+            }
         } else {
             System.out.println("Вы ввели не число");
         }
-
 
     }
 }
